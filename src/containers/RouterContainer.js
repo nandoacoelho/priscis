6463c0e4121d72126b57113c933c1e7a827da89b
Fernando de Almeida Coelho <nandoacoelho@gmail.com>
2018-04-15 10:56:04 -0300

    Add animations for home

--- src/containers/RouterContainer.js
+++ src/containers/RouterContainer.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Home from "./Home";
 import Work from "./Work";
 import About from "./About";
 import Contact from "./Contact";
+
 import { WORK, CONTACT, ABOUT, HOME } from "../constants";
 
 class RouterContainer extends Component {
